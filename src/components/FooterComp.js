4037ae6dac45e29adfd82b6e6298a0a0a465bbe0
Kevin98 <kevinryner@yahoo.co.uk>
2021-10-31 19:11:37 +0000

    hrefs  for logo sset to text decoration none

--- src/components/FooterComp.js
+++ src/components/FooterComp.js
@@ -12,17 +12,17 @@ export default function FooterComp() {
           <footer style = {{backgroundColor: "#291F1E", color: "white"}} className="d-flex flex-wrap justify-content-center align-content-center border rounded w-100 py-2">
             <div className = "d-flex p-1 align-items-center">
             <h3 className="px-2"> GitHub:  </h3>
-            <a href = "https://github.com/kevinjr1998" target="_blank" rel = "noreferrer"  className = "smLink rounded d-flex" ><i className="fab fa-github fa-3x"></i></a>
+            <a href = "https://github.com/kevinjr1998" target="_blank" rel = "noreferrer"  className = "smLink rounded d-flex text-decoration-none" ><i className="fab fa-github fa-3x"></i></a>
             </div>
 
             <div className = " d-flex p-1 align-items-center">
             <h3 className="px-2"> LinkedIn:  </h3>
-            <a href = "https://www.linkedin.com/in/kevin-ryner/" target="_blank" rel = "noreferrer" className = "smLink rounded d-flex" ><i className="fab fa-linkedin fa-3x"></i></a>
+            <a href = "https://www.linkedin.com/in/kevin-ryner/" target="_blank" rel = "noreferrer" className = "smLink rounded d-flex text-decoration-none" ><i className="fab fa-linkedin fa-3x"></i></a>
             </div>
 
             <div className = "d-flex p-1 mx-2 w-30 align-items-center">
             <h3 className="px-2"> StackOverflow:  </h3>
-            <a href = "https://stackoverflow.com/users/15321370/" target="_blank" rel = "noreferrer" className = "smLink rounded d-flex" ><i className="fab fa-stack-overflow fa-3x "></i></a>
+            <a href = "https://stackoverflow.com/users/15321370/" target="_blank" rel = "noreferrer" className = "smLink rounded d-flex text-decoration-none" ><i className="fab fa-stack-overflow fa-3x "></i></a>
             </div>
             </footer>
         </>
