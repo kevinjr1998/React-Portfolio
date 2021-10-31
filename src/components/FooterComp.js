4d439af016abc7a9f668020dc1c297ea726461e4
Kevin98 <kevinryner@yahoo.co.uk>
2021-10-31 19:06:55 +0000

    logos updated

--- src/components/FooterComp.js
+++ src/components/FooterComp.js
@@ -1,8 +1,8 @@
 import {React} from "react";
 
-import gitHubLogo from "./images/GitHubLogo.png"
-import linkedInLogo from "./images/LinkedInLogo.png"
-import stackOverflowLogo from "./images/stackOverflowLogo.png"
+// import gitHubLogo from "./images/GitHubLogo.png"
+// import linkedInLogo from "./images/LinkedInLogo.png"
+// import stackOverflowLogo from "./images/stackOverflowLogo.png"
 
 
 export default function FooterComp() {
@@ -10,19 +10,19 @@ export default function FooterComp() {
     return (
         <>
           <footer style = {{backgroundColor: "#291F1E", color: "white"}} className="d-flex flex-wrap justify-content-center align-content-center border rounded w-100 py-2">
-            <div className = "d-flex p-1">
+            <div className = "d-flex p-1 align-items-center">
             <h3 className="px-2"> GitHub:  </h3>
-            <a href = "https://github.com/kevinjr1998" target="_blank" rel = "noreferrer"  className = "smLink rounded d-flex" ><img src={gitHubLogo} className="img-fluid rounded" alt="GitHub Logo" style ={{width: 60, aspectRatio: 3/2,  backgroundColor: 'transparent' }}></img></a>
+            <a href = "https://github.com/kevinjr1998" target="_blank" rel = "noreferrer"  className = "smLink rounded d-flex" ><i className="fab fa-github fa-3x"></i></a>
             </div>
 
-            <div className = " d-flex p-1 ">
+            <div className = " d-flex p-1 align-items-center">
             <h3 className="px-2"> LinkedIn:  </h3>
-            <a href = "https://www.linkedin.com/in/kevin-ryner/" target="_blank" rel = "noreferrer" className = "smLink rounded d-flex" ><img src={linkedInLogo} className="img-fluid rounded smLink" alt="LinkedIn Logo" style ={{width: 40, aspectRatio: 3/2,  backgroundColor: 'transparent'}}></img></a>
+            <a href = "https://www.linkedin.com/in/kevin-ryner/" target="_blank" rel = "noreferrer" className = "smLink rounded d-flex" ><i className="fab fa-linkedin fa-3x"></i></a>
             </div>
 
-            <div className = "d-flex p-1 mx-2 w-30">
+            <div className = "d-flex p-1 mx-2 w-30 align-items-center">
             <h3 className="px-2"> StackOverflow:  </h3>
-            <a href = "https://stackoverflow.com/users/15321370/" target="_blank" rel = "noreferrer" className = "smLink rounded d-flex" ><img src={stackOverflowLogo} className="img-fluid rounded smLink" alt="StackOverflow Logo" style ={{width: 70, aspectRatio: 3/2,  backgroundColor: 'transparent'}}></img></a>
+            <a href = "https://stackoverflow.com/users/15321370/" target="_blank" rel = "noreferrer" className = "smLink rounded d-flex" ><i className="fab fa-stack-overflow fa-3x "></i></a>
             </div>
             </footer>
         </>
