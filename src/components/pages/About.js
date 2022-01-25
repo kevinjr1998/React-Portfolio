74694051886fdcc8a55576f2e1cc11f91806e995
Kevin98 <kevinryner@yahoo.co.uk>
2022-01-25 14:45:12 +0000

    updated cv and about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -10,10 +10,9 @@ export default function About() {
       <img src={headshot} className="img-fluid border rounded d-inline-flex flex-wrap my-3"  style ={{height: 280, aspectRatio: 5/2,  backgroundColor: 'transparent'}} alt="Headshot"/>
       <p className = "d-flex flex-wrap">
         I am a junior full-stack web developer with a background in physics
-        looking to leverage my problem solving skills. I am currently working 
-        towards earning a certificate in full stack development from the University 
+        looking to leverage my problem solving skills. I have earned a certificate in full stack development from the University 
         Coding Bootcamp, hosted by University of Birmingham. I have knowledge and skills
-        in both front end technologies such as ReactJS, HTML and Bootstrap, but also 
+        in front end technologies such as ReactJS, HTML and Bootstrap, and also 
         several back end technologies, like Node.JS, MongoDB and MySQL.
       </p>
       </div>
